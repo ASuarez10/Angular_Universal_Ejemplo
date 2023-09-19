3353199483d118e4c02c8d2b51a7193b79c1b953
ALEJANDRO SUAREZ BEJARANO <1007707024@bolivar.com>
2023-09-19 14:25:07 -0500

    First commit with the instalation of Angular Universal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page404Component } from './shared/page404/page404.component';
+import { HomeModule } from './modules/home/home.module';
+import { ContactModule } from './modules/contact/contact.module';
+import { AboutModule } from './modules/about/about.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => HomeModule
+  },
+  {
+    path: 'about',
+    loadChildren: () => AboutModule
+  },
+  {
+    path: 'contact',
+    loadChildren: () => ContactModule
+  },
+  {
+    path: '**', component: Page404Component
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/modules/about/componente01/componente01.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-about-01',
+  templateUrl: './componente01.component.html',
+  styleUrls: ['./componente01.component.css']
+})
+export class Componente01Component {
+
+}
